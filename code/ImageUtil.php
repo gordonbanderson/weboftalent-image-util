771720bed4480e005443475af5244bb034dafc19
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-09-23 17:29:10 +0700

    FIX: Fixed methods that were not working when writing the README

--- code/ImageUtil.php
+++ code/ImageUtil.php
@@ -7,68 +7,104 @@ class ImageUtil extends Extension {
         return array();
     }
 
-    public function generateRotateClockwise(GD $gd) {
-        return $gd->rotate(90);
+
+    /*
+    Rotate an image clockwise
+    */
+    public function RotateClockwise() 
+    {
+        return $this->owner->getFormattedImage('RotateClockwise');
     }
-     
-    public function generateRotateCounterClockwise(GD $gd)  {
+
+    /*
+    Rotates an image clockwise, called from $RotateClockwise in a template
+    */
+    public function generateRotateClockwise(GD $gd) {
         return $gd->rotate(270);
     }
-     
+
+    /*
+    Rotate an image anti clockwise
+    */
+    public function RotateAntiClockwise() 
+    {
+        return $this->owner->getFormattedImage('RotateAntiClockwise');
+    }
+
+    /*
+    Rotates an image clockwise, called from $RotateAntiClockwise in a template
+    */
+    public function generateRotateAntiClockwise(GD $gd)  {
+        return $gd->rotate(90);
+    }
+
     public function clearResampledImages()  {
         $files = glob(Director::baseFolder().'/'.$this->Parent()->Filename."_resampled/*-$this->Name");
         foreach($files as $file) {unlink($file);}
     }
-     
+    
+    /*
+    Check whether or not an image is landscape aspect
+    */
     public function IsLandscape() {
-        return $this->getWidth() > $this->getHeight();
+        return $this->owner->getWidth() > $this->owner->getHeight();
     }
-     
+    
+
+    /*
+    Check whether not an image is portrait aspect
+    */
     public function IsPortrait() {
-        return $this->getWidth() < $this->getHeight();
+        return $this->owner->getWidth() < $this->owner->getHeight();
+    }
+    
+
+    /*
+    Generate a padded image with the padding background color configurable.  Silverstripe's default is white
+    */ 
+    function generatePaddedImageWithColor(GD $gd,$colorWidth="fff 600 400"){
+        $Vars = explode(' ', $colorWidth); 
+        $height = $Vars[2];
+        $width = $Vars[1];
+        $color = $Vars[0];
+        return $gd->paddedResize($width, $height,$color);
+    }
+
+    public function PaddedImageWithColor($colorWidthHeight) 
+    {
+        return $this->owner->getFormattedImage('PaddedImageWithColor', $colorWidthHeight);
     }
      
-    function generatePaddedImageByWidth(GD $gd,$width=600,$color="fff"){
-        return $gd->paddedResize($width, round($gd->getHeight()/($gd->getWidth()/$width),0),$color);
-    }
-     
-    public function Exif(){
+    public function ExifData(){
         //http://www.v-nessa.net/2010/08/02/using-php-to-extract-image-exif-data
-        $image = $this->AbsoluteURL;
+        $image = $this->owner->AbsoluteURL;
         $d=new DataObjectSet(); 
         $exif = exif_read_data($image, 0, true);
         foreach ($exif as $key => $section) {
             $a=new DataObjectSet(); 
-            foreach ($section as $name => $val)
-                $a->push(new ArrayData(array("Title"=>$name,"Content"=>$val)));
-            $d->push(new ArrayData(array("Title"=>strtolower($key),"Content"=>$a)));
+            foreach ($section as $name => $val) {
+                $a->push(new ArrayData(array("Name"=>$name,"Value"=>$val)));
+            }
+            $d->push(new ArrayData(array("Name"=>strtolower($key),"Value"=>$a)));
         }
         return $d;
     }
 
 
 	 
-	public function generateGreyscaleImage(GD $gd, $RGB) 
-	{
-	    $Vars = explode(' ', $RGB);     
-	     
-	    return $gd->greyscale( $Vars[0], $Vars[1], $Vars[2]);
-	}
 
 
-		/* Greyscale image */
-	public function GreyscaleImage($RGBW = '30 30 30 200') 
+	/* Greyscale image */
+	public function Greyscale($RGBW = '30 30 30 100') 
 	{
-		//error_log("CUSTOM IMAGE T1");
-	    return $this->owner->getFormattedImage('PromotedImage', $RGBW);
+	    return $this->owner->getFormattedImage('GreyscaleWithWidth', $RGBW);
 	}
+
+    
 	 
-	public function generatePromotedImage(GD $gd, $RGBW) 
+	public function generateGreyscaleWithWidth(GD $gd, $RGBW) 
 	{
 	    $Vars = explode(' ', $RGBW);
-
-
-	    error_log(print_r($Vars,1));
 	   	
 	   	// resize the image
 		$result  = $gd->resizeByWidth($Vars[3]);
